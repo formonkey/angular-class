2f63f702ccf76106e76f7a57e0314e088a6dbe9f
formonkey <misaacmartin@gmail.com>
2019-02-27 12:33:32 +0100

    Change graphql db

--- server/graphql/db.js
+++ server/graphql/db.js
@@ -1,22 +1,18 @@
 module.exports = {
-    posts: [
-        { id: 1, title: "Lorem Ipsum", views: 254, user_id: 123, player_id: 1 },
-        { id: 2, title: "Sic Dolor amet", views: 65, user_id: 456, player_id: 2 },
-    ],
     users: [
-        { id: 123, name: "John Doe" },
-        { id: 456, name: "Jane Doe" }
+        {id: 1, username: 'skullpanda', password: 'test123', profile_id: 1},
+        {id: 2, username: 'noevite', password: 'test123', profile_id: 2}
     ],
-    players: [
-        { id: 1, name: "John Doe" },
-        { id: 2, name: "Jane Doe" }
+    profiles: [
+        {id: 1, role: 'ADMIN', name: 'Isaac', surname: 'Martin'},
+        {id: 2, role: 'ADMIN', name: 'Noelia', surname: 'Villa'}
+    ],
+    posts: [
+        {id: 1, title: "Lorem Ipsum", views: 254, profile_id: 1},
+        {id: 2, title: "Sic Dolor amet", views: 65, profile_id: 1},
     ],
     comments: [
-        { id: 987, post_id: 1, body: "Consectetur adipiscing elit", date: new Date('2017-07-03') },
-        { id: 995, post_id: 1, body: "Nam molestie pellentesque dui", date: new Date('2017-08-17') }
+        {id: 1, post_id: 1, body: "Consectetur adipiscing elit", date: new Date('2017-07-03'), user_id: 2},
+        {id: 2, post_id: 1, body: "Nam molestie pellentesque dui", date: new Date('2017-08-17'), user_id: 3}
     ],
-    tests: [
-        { id: 987, post_id: 1, text: "Test 1" },
-        { id: 995, post_id: 1, text: "Test 2" }
-    ]
 };
