a68eed4e788968ea21ea611a1aceafd355a4951d
zapico <pescoyalosdeberes@gmail.com>
2019-03-03 13:29:16 +0100

    Updated header to use search

--- client/src/app/shared/header/header.module.ts
+++ client/src/app/shared/header/header.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 
+import { SearchModule } from "../search";
 import { HeaderComponent } from "./components";
 import { NavBarModule } from "../../elements/nav-bar";
 
 @NgModule({
     imports: [
         NavBarModule,
+        SearchModule,
     ],
     declarations: [
         HeaderComponent,
