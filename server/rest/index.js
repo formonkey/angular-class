c094d5bdbd033991bd1bf8eeaa9de5f7c8b15770
formonkey <misaacmartin@gmail.com>
2019-03-03 21:32:28 +0100

    ADD test configuration

--- server/rest/index.js
+++ server/rest/index.js
@@ -2,9 +2,11 @@ const posts = require('../data/posts');
 const users = require('../data/users');
 const profiles = require('../data/profiles');
 
-module.exports = {
-    users,
-    profiles,
-    posts,
-    comments,
+module.exports = function() {
+    return {
+        users,
+        profiles,
+        posts,
+        comments,
+    }
 };
